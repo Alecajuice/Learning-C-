57df3f85421d1626da4ab37d86f4eac0bf6aafee
Alex Tarng <alecadad@gmail.com>
2015-09-10 07:34:53 -0700

    what the fuck

---
+++ Learning C++/Main.cpp
@@ -0,0 +1,21 @@
+/*
+ * Main.cpp
+ *
+ *  Created on: Sep 10, 2015
+ *      Author: Alecajuice
+ */
+
+#include "Main.h"
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "Enter ur name nigga" << endl;
+
+	string name;
+	cin >> name;
+
+	cout << "yo wassup " << name << endl;
+	return 0;
+}
---
+++ Learning C++/Main.h
@@ -0,0 +1,14 @@
+/*
+ * Main.h
+ *
+ *  Created on: Sep 10, 2015
+ *      Author: Alecajuice
+ */
+
+#ifndef MAIN_H_
+#define MAIN_H_
+
+class Main {
+};
+
+#endif /* MAIN_H_ */
